568ea02bea6d19246a83dc74a97b66de1f96af9c
Edy Ridwan Hutagalung <68416754+Dotclang@users.noreply.github.com>
2023-06-30 19:33:04 +0700

    revise packages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    // vite config
+    server: {
+      https: "https://dotclang.github.io/frontend-app-react",
+    },
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+    plugins: [react()],
+  };
 });
