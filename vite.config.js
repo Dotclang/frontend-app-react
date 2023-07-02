85ed34c3be757c5e387ff5f1f44ba1ef429b06ce
Edy Ridwan Hutagalung <68416754+Dotclang@users.noreply.github.com>
2023-07-02 13:34:16 +0700

    revise config vites

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/frontend-app-react/",
+  base: process.env.NODE_ENV === "production" ? "/frontend-app-react/" : "./",
   plugins: [react()],
 });
