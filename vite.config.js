b8a90941c41c726387426c42d78bb886f9e682a5
Edy Ridwan Hutagalung <68416754+Dotclang@users.noreply.github.com>
2023-07-02 13:11:23 +0700

    revise deployments

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/frontend-app-react/" : "./",
-  build: {
-    outDir: "dist",
-    assetsDir: "./",
-    rollupOptions: {
-      input: "./src/main.jsx",
-    },
-  },
 });
