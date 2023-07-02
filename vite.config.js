4e0427ea33724d8e5f42552bde1c6bf5d47d93a1
Edy Ridwan Hutagalung <68416754+Dotclang@users.noreply.github.com>
2023-07-02 12:30:40 +0700

    revise vit config

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,14 @@
-import { resolve } from "path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-
-// export default defineConfig(({ command, mode }) => {
-//   const env = loadEnv(mode, process.cwd(), "VITE_");
-
-//   console.log(env);
-//   return {
-//     build: {
-//       rollupOptions: {
-//         input: {
-//           main: resolve(__dirname, "index.html"),
-//         },
-//       },
-//     },
-//     define: {
-//       __APP_ENV__: env.APP_ENV,
-//     },
-//     plugins: [react()],
-//   };
-// });
-
 export default defineConfig({
-  server: {
-    base: "https://dotclang.github.io/frontend-app-react/",
-  },
   plugins: [react()],
+  base: process.env.NODE_ENV === "production" ? "/frontend-app-react/" : "./",
+  build: {
+    outDir: "dist",
+    assetsDir: "./",
+    rollupOptions: {
+      input: "./src/main.jsx",
+    },
+  },
 });
